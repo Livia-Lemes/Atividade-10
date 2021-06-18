da12a7ab672192f3d0ed3e131e039a13dc1c206d
Livia-Lemes <80851755+Livia-Lemes@users.noreply.github.com>
2021-06-18 19:10:26 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+let h1 = document.querySelector('#meuH1');
+
+
+let main = document.querySelector('main');
+
+const div = document.querySelector('div');
+div.insertAdjacentHTML('afterbegin', '<div>Eu tenho a natureza, a arte e a póesia, e se isso não for o suficiente, o que é? </div>');
+div.insertAdjacentHTML('beforeend', '<div> -Vicent Van Gogh </div>');
+
+
+
+//let div1 = document.createElement('div');
+
+//let primeiroH2 = 
+
+//let primeiroP = 
+
+//div1.appendChild(primeiroH2);
+//div1.appendChild(primeiroP);
+//MediaDeviceInfo.appendChild(div1)
+
+//let div2 = document.createElement('div');
+
+//let segundoH2 = 
+
+//let segundoP = 
+
+//div2.appendChild(segundoH2);
+//div2.appendChild(segundoP);
+//MediaDeviceInfo.appendChild(div2)
